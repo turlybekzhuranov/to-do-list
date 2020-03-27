fb742c5941c4e72e8d4db41a1d784f6b6a078fe5
turlybek <turlybekk@gmail.com>
2020-03-27 21:37:58 +0600

    get tasks from db and show tasks

--- models/tasks.php
+++ models/tasks.php
@@ -1,1 +1,4 @@
 <?php
+    require 'config/db.php';
+
+    $tasks = $pdo->query('SELECT * FROM `tasks` ORDER BY `id` DESC');
--- views/showtasks.php
+++ views/showtasks.php
@@ -1,1 +1,26 @@
 <?php
+    require 'models/tasks.php';
+
+    echo '<div class="container">';
+        echo '<table class="table table-bordered table-striped">';
+            echo "<thead>";
+                echo '<tr>
+                       <th class="col-md-2"><a href="/sort.php">Имя пользователя</a></th>
+                       <th class="col-md-2"><a href="/sort.php">Email</a></th>
+                       <th class="col-md-2"><a href="/sort.php">Текст</a></th>
+                       <th class="col-md-2"><a href="/sort.php">Статус</a></th>
+                    </tr>';
+            echo '</thead>';
+            echo '<tbody>';
+            while ($task = $tasks->fetch(PDO::FETCH_OBJ)){
+                echo '<tr>';
+                    echo '<td>' . $task->name . '</td>';
+                    echo '<td>' . $task->email . '</td>';
+                    echo '<td>' . $task->text . '</td>';
+                    echo '<td>' . $task->status . '</td>';
+                 echo '</tr>';
+            }
+            echo '</tbody>';
+        echo '</table>';
+    echo '</div>';
+
